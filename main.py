1a07c812bdafa1cfea21597bc6b1f8280639cf2c
felixjkw <post2felix@gmail.com>
2025-09-22 17:43:14 +0200

    added transfer_spec

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sys.path.insert(0, str(sdk_path))
 try: 
     import transferd_api.transferd_pb2 as transfer_manager
     import transferd_api.transferd_pb2_grpc as transfer_manager_grpc
-    print("Import successfull ✅")
+    #print("✅ Import successfull")
 
 except ImportError:
     print("Protobuf installation needed to run this code")
@@ -32,7 +32,6 @@ class VideoUploader:
         self.remote_host = os.environ.get("ASPERA_REMOTE_HOST", "https://ats-sl-fra.aspera.io:443")
         self.destination = os.environ.get("COS_DESTINATION", "/aspera-uploads")
         self._validatate_environment()
-        print("Confiuration loaded successfully ✅")
 
     def _validatate_environment(self):
         required_vars = [
@@ -46,6 +45,8 @@ class VideoUploader:
             print("Missing required environment variables:")
             for var in missing_vars:
                 print(f"Set {var} before running again")
+        else: 
+            print("✅ Confiuration loaded successfully")
 
 def find_video_files(self, directory):
     video_types = [".mp4", ".mov", ".MP4", ".MOV"]
@@ -56,15 +57,49 @@ def find_video_files(self, directory):
 
     for video in directory.rglob("*"):
         if video.suffix in video_types and video.is_file():
-            video_files.append(video)
+            video_files.append(str(video))
     
     print(f"Found {len(video_files)} video files")
     return video_files
 
+def create_transfer_spec(self, filepaths):
+    paths = []
+
+    for filepath in filepaths:
+        paths.append({"source": filepath})
+
+    trasfer_spec = {
+        "session_initialization": {
+            "icons": {
+                "api_key": self.api_key,
+                "bucket": self.bucket,
+                "ibm_service_instance_id": self.service_instance_id,
+                "ibm_service_endpoint": self.service_endpoint
+            }
+        },
+
+        "direction": "send",
+        "remote_host": self.remote_host,
+        "title": "video file upload",
+
+        "assets": {
+            "destination_root": self.destination,
+            "paths": paths,
+        },
+    }
+    return json.dumps(trasfer_spec, indent=2)
+
+
+
 def main():
-    print("Starting Video Uploader ... 🔄")
+    print("🔄 Starting Video Uploader ...")
+    
     uploader = VideoUploader()
-    find_video_files(uploader, directory=Path(__file__).parent)
+    
+    video_files_finder_execute = find_video_files(uploader, directory=Path(__file__).parent)
+    for file in video_files_finder_execute:
+        print(" 📹 Found Video:", file)
+    
     
              
 if __name__ == "__main__":
